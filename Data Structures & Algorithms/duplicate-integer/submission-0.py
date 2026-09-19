76dbe3a0f1116a161e6dee58b16a0de18214c7b5
Youngsang-Cho1 <youngsang1211@gmail.com>
2026-09-18 20:40:32 -0400

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        c = Counter(nums)
+        for key,val in c.items():
+            if val > 1:
+                return True
+        return False
+
+        
