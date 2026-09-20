9ac5a932cb66dee521350ccf744415056db62e7f
Youngsang-Cho1 <youngsang1211@gmail.com>
2026-09-20 15:42:11 -0400

    Update: house-robber-ii - submission-4

--- Data Structures & Algorithms/house-robber-ii/submission-4.py
+++ Data Structures & Algorithms/house-robber-ii/submission-4.py
@@ -1,25 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        elif len(nums) == 2:
-            return max(nums[0], nums[1])
-        dp_first = [0 for _ in range(len(nums))]
-        dp_first[0] = nums[0]
-        dp_first[1] = nums[0] if nums[0] > nums[1] else nums[1]
-        dp_last = [0 for _ in range(len(nums))]
-        dp_last[1] = nums[1]
-        dp_last[2] = nums[1] if nums[1] > nums[2] else nums[2]
+        dp = []
+        if len(nums) <= 3:
+            return max(nums)
+        dp1 = nums[:-1]
+        dp1[1] = max(dp1[0], dp1[1])
+        dp2 = nums[1:]
+        dp2[1] = max(dp2[0], dp2[1])
+        for i in range(2,len(dp1)):
+            dp1[i] = max(dp1[i-2] + dp1[i], dp1[i-1])
+        print(dp1)
 
-        for i in range(2, len(dp_first)):
-            dp_first[i] = max(dp_first[i-1], dp_first[i-2] + nums[i])
+        for i in range(2, len(dp2)):
+            dp2[i] = max(dp2[i-2] + dp2[i], dp2[i-1])
+        print(dp2)
+        return max(dp1[-1], dp2[-1])
+
+
+
         
-        for j in range(3, len(dp_last)):
-            dp_last[j] = max(dp_last[j-1], dp_last[j-2] + nums[j])
-            print(dp_last)
 
-        return(max(dp_first[-2], dp_last[-1]))
             
         
