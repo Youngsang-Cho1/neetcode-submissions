833a97e85282e1ffbe7d74b97ef50e0882e4cbcc
Youngsang-Cho1 <youngsang1211@gmail.com>
2026-09-25 14:29:30 -0400

    Update: partition-labels - submission-0

--- Data Structures & Algorithms/partition-labels/submission-0.py
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -1,16 +1,32 @@
+from collections import Counter, defaultdict
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last = {}
-        for idx, i in enumerate(s):
-            last[i] = idx
+        # lowercase letters
+        # as many substring as possible
+        # each letter in only one substring
+        # return list of ints w size of substrings
         res = []
-        length = end = 0
-        for idx, i in enumerate(s):
-            length += 1
-            end = max(end, last[i])
+        curr = 1
+        original_counter = Counter(s)
+        curr_counter = defaultdict(int)
 
-            if idx == end:
-                res.append(length)
-                length = 0
+        for i in range(len(s)):
+            curr_counter[s[i]] += 1
+            is_same = True
+            for key, val in curr_counter.items():
+                if curr_counter[key] != original_counter[key]:
+                    is_same = False
+                    break
+            if is_same:
+                res.append(curr)
+                start = i
+                curr = 0
+            curr += 1
+
+        if start == 0:
+            res.append(len(s))
+        
         return res
-            
+
+
+
